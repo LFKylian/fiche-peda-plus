000a4dcbb0ba540b7b816210502764e24f2ef4e0
LFKylian <kylian.lefevre.1@ens.etsmtl.ca>
2026-01-28 19:39:31 +0000

    Trying an other route configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/fiche-peda-plus",
+  // base:"/fiche-peda-plus",
 
   plugins: [
     react(),
